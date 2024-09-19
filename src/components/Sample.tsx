de1048295649b448d7898f6ebf54733408502727
Mary Songal <songalmary@gmail.com>
2024-09-19 07:59:35 +0200

    002 Initial Project Setup: Added ESLint configs and Prettier

--- src/components/Sample.tsx
+++ src/components/Sample.tsx
@@ -2,7 +2,7 @@ import "./Sample.css";
 
 export default function Sample() {
   return (
-    <footer>
+    <footer color="red">
       <a href="#">Тестовый Footer для настройки Webpack+TS</a>
     </footer>
   );
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,36 +3,34 @@ import { Configuration } from "webpack";
 import CopyWebpackPlugin from "copy-webpack-plugin";
 
 const config: Configuration = {
-    mode:
-      (process.env.NODE_ENV as "production" | "development" | undefined) ??
-      "development",
-    entry: "./src/main.tsx",
-    module: {
-      rules: [
-        {
-          test: /.tsx?$/,
-          use: "ts-loader",
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"],
-        },
-      ],
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-    },
-    output: {
-      filename: "bundle.js",
-      path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [
-        // Использован для простоты (HtmlWebpackPlugin можно использовать, если проект покрупней)
-      new CopyWebpackPlugin({
-        patterns: [{ from: "public" }],
-      }),
+  mode: (process.env.NODE_ENV as "production" | "development" | undefined) ?? "development",
+  entry: "./src/main.tsx",
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-  };
-  
-  export default config;
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    // Использован для простоты (HtmlWebpackPlugin можно использовать, если проект покрупней)
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public" }],
+    }),
+  ],
+};
+
+export default config;
