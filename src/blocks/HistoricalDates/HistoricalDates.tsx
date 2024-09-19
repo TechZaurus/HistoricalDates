4b0791fac0ff5a2632e64a9d508abad7a7fc83a1
Mary Songal <songalmary@gmail.com>
2024-09-19 12:54:42 +0200

    004: Feature Historical Dates: Added crosshair inside container

--- src/blocks/HistoricalDates/HistoricalDates.tsx
+++ src/blocks/HistoricalDates/HistoricalDates.tsx
@@ -4,7 +4,17 @@ import { historicalDates } from "./HistoricalDates.module.scss";
 const HistoricalDates = () => {
   return (
     <div className={historicalDates}>
-      <Container bordered>Тест</Container>
+      <Container
+        bordered
+        addCrosshair
+        style={{
+          marginLeft: "15%",
+          marginRight: "10%",
+          height: "100%"
+        }}
+      >
+        Тест
+      </Container>
     </div>
   );
 };
--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,17 +1,29 @@
 import { PropsWithChildren, ReactNode } from "react";
 import classNames from "classnames";
-import { borderedContainer } from "./Container.module.scss";
+import {
+  borderedContainer,
+  container__HorizontalLine,
+  container__VerticalLine,
+} from "./Container.module.scss";
 
 interface Props extends PropsWithChildren {
   bordered?: boolean;
+  style?: React.CSSProperties;
   children?: ReactNode;
+  addCrosshair?: boolean;
 }
 
-const Container: React.FC<Props> = ({ bordered, children }) => {
+const Container: React.FC<Props> = ({ bordered, addCrosshair, children, style }) => {
   const classNameObject: any = {};
   classNameObject[borderedContainer] = bordered;
-  const containerClass = classNames(classNameObject);
-  return <div className={containerClass}>{children}</div>;
+
+  return (
+    <div style={style} className={classNames(classNameObject)}>
+      <div style={{ position: "absolute" }}>{children}</div>
+      {addCrosshair && <div className={container__HorizontalLine} />}
+      {addCrosshair && <div className={container__VerticalLine} />}
+    </div>
+  );
 };
 
 export default Container;
